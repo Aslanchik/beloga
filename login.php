7b3234ad4b5e919f0225e386fa227b7dce7b71f6
aslanchik <aslanchikgw2@hotmail.com>
2020-07-30 10:32:11 +0300

    completed login form

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ $page_title = 'Login Page'
             <div class="row">
                 <div class="col-12 mt-3">
                     <h1 class="display-4">
-                        LOGIN TOY OUR USER SO YOU CAN SEE FISH
+                        LOGIN TO YOUR USER SO YOU CAN SEE FISH
                     </h1>
                     <p><a href="register.php">ABOUT FISH BELUGA FISH FISH YES</a></p>
                 </div>
@@ -18,7 +18,19 @@ $page_title = 'Login Page'
         <section id="login-form">
             <div class="row">
                 <div class="col-lg-6">
-
+                    <form method="POST" action="" id="login" autocomplete="off" novalidate>
+                        <div class="form-group">
+                            <label for="email">* Email</label>
+                            <input class="form-control" type="email" name="email" id="email">
+                            <span class="text-danger"></span>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">* Password</label>
+                            <input class="form-control" type="password" name="password" id="password">
+                            <span class="text-danger"></span>
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-dark">LOGIN</button>
+                    </form>
                 </div>
             </div>
         </section>
